3fbb068fdd9de966f88d466d4dd1bb7ef6376bde
Daniel Kinyo <kinyodaniel@gmail.com>
2022-09-23 23:31:10 +0300

    remove port from nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,13 +6,13 @@ const path = require('path')
 const ENV_DEV = true
 
 // Development Environment
-let port = 3000
+// let port = 3000
 let host = 'localhost'
 let https = false
 
 // Production Environment
 if (ENV_DEV === false) {
-  port = 3000 // make sure this port is open on your server you can do that via WHM or talk to you hosting company
+  // port = 3000 // make sure this port is open on your server you can do that via WHM or talk to you hosting company
   // eslint-disable-next-line no-unused-vars
   host = 'wankimani.ac.ke'
   // eslint-disable-next-line no-unused-vars
@@ -30,9 +30,13 @@ export default {
   ssr: true,
   target: 'server',
 
+  // server: {
+  //   host: '0.0.0.0',
+  //   port,
+  //   timing: false,
+  // },
   server: {
     host: '0.0.0.0',
-    port,
     timing: false,
   },
   static: {
